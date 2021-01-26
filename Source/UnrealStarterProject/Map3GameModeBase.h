a6d946e8cfa69d724adbc830fd9ef2632d6c6708
jfcarocota <jfcarocota@gmail.com>
2021-01-26 15:27:17 -0700

    Character class

---
+++ Source/UnrealStarterProject/Map3GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Map3GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALSTARTERPROJECT_API AMap3GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
